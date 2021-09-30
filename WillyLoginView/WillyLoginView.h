2bd7adcd85534625815b254820cf63adde09b612
wdesimini <wdesimini8@gmail.com>
2021-09-30 15:16:51 -0700

    Initial Commit

---
+++ WillyLoginView/WillyLoginView.h
@@ -0,0 +1,18 @@
+//
+//  WillyLoginView.h
+//  WillyLoginView
+//
+//  Created by Wilson Desimini on 9/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WillyLoginView.
+FOUNDATION_EXPORT double WillyLoginViewVersionNumber;
+
+//! Project version string for WillyLoginView.
+FOUNDATION_EXPORT const unsigned char WillyLoginViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WillyLoginView/PublicHeader.h>
+
+
