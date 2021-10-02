748224d26deb672f61a35c6966d0c124bf186682
Wilson Desimini <wdesimini8@gmail.com>
2021-10-01 17:12:52 -0700

    implement signup view-model in signup view

--- WillyLoginView/Modules/Signup/WLYSignupView.swift
+++ WillyLoginView/Modules/Signup/WLYSignupView.swift
@@ -12,48 +12,82 @@ struct WLYSignupView: View {
     @State var email = ""
     @State var password = ""
     @State var username = ""
+    @ObservedObject var viewModel: WLYSignupViewModel
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 32) {
-            Text("Let's get started")
-                .textStyle(WLYTitleTextStyle())
-            WLYTitledTextField(
-                detailButtonModel: nil,
-                placeholder: "Email",
-                title: "Email",
-                text: $email
-            )
-            WLYTitledTextField(
-                detailButtonModel: nil,
-                placeholder: "Username",
-                title: "Username",
-                text: $username
-            )
-            WLYTitledTextField(
-                detailButtonModel: nil,
-                placeholder: "8 characters, uppercase",
-                title: "Password",
-                text: $password
-            )
-            Spacer()
+        if let signedUp = viewModel.signUpResult {
+            Text(signedUp ? "Signed up!" : "Couldn't sign up!")
+        } else if let signUpError = viewModel.signUpError {
+            Text("Error logging in - \(signUpError.localizedDescription)")
+        } else if viewModel.signingUp {
             VStack {
-                WLYCheckbox(
-                    checked: $agreeToTerms,
-                    text: "By creating an account, you agree to the terms of service and our privacy policy"
-                )
-                Button("Sign up", action: signUpTapped)
-                    .buttonStyle(WLYProminentButtonStyle())
+                Text("Signing up")
+                ProgressView()
             }
+        } else {
+            VStack(alignment: .leading, spacing: 32) {
+                Text("Let's get started")
+                    .textStyle(WLYTitleTextStyle())
+                WLYTitledTextField(
+                    detailButtonModel: nil,
+                    placeholder: "Email",
+                    title: "Email",
+                    text: $email
+                )
+                WLYTitledTextField(
+                    detailButtonModel: nil,
+                    placeholder: "Username",
+                    title: "Username",
+                    text: $username
+                )
+                WLYTitledTextField(
+                    detailButtonModel: nil,
+                    placeholder: "8 characters, uppercase",
+                    title: "Password",
+                    text: $password
+                )
+                Spacer()
+                VStack {
+                    WLYCheckbox(
+                        checked: $agreeToTerms,
+                        text: "By creating an account, you agree to the terms of service and our privacy policy"
+                    )
+                    Button("Sign up", action: signUpTapped)
+                        .buttonStyle(WLYProminentButtonStyle())
+                }
+            }
+            .padding()
         }
-        .padding()
     }
     
     private func signUpTapped() {
+        viewModel.signUp(
+            agreedToTerms: agreeToTerms,
+            email: email,
+            username: username,
+            password: password
+        )
     }
 }
 
 struct WLYSignupView_Previews: PreviewProvider {
     static var previews: some View {
-        WLYSignupView()
+        let service = WLYSignupServiceSample()
+        let viewModel = WLYSignupViewModel(service: service)
+        return WLYSignupView(viewModel: viewModel)
+    }
+}
+
+struct WLYSignupServiceSample: WLYSignupService {
+    func signUp(
+        agreedToTerms: Bool,
+        email: String,
+        username: String,
+        password: String,
+        completion: @escaping Handler
+    ) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            completion(.success(true))
+        }
     }
 }
